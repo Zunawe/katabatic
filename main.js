e541044b5affc9e1929a9d2b42706c8406bb2b5e
Zunawe <gyroscope15@gmail.com>
2017-08-09 13:28:09 -0600

    Broke out wind to its own object, added variation in trees, and changed blowing animation slightly

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var phi = (1 + Math.sqrt(5)) / 2;
 var dt = 0.02;
-var windSpeed = 20;
+var windSpeed = 5;
 
 function App(){
 	var _this = this;
@@ -10,7 +10,12 @@ function App(){
 		_this.ctx = _this.canvas.getContext('2d');
 
 		_this.trees = [];
-		_this.trees.push(new Tree(_this.ctx, {x: 500, y: 500, scale: 200}));
+		for(var i = 0; i < 20; ++i){
+			let depth = Math.random() * 100;
+			_this.trees.push(new Tree(_this.ctx, {x: 100 + (30 * (i + Math.random())), y: 200 - depth, scale: 60 * (1 - (depth / 100)) + 10}));			
+		}
+
+		_this.wind = new Wind(windSpeed);
 
 		_this.ctx.canvas.width = window.innerWidth;
 		_this.ctx.canvas.height = window.innerHeight;
@@ -26,17 +31,18 @@ function App(){
 		_this.ctx.fillStyle = '#4B5';
 
 		var d = new Date();
-		_this.trees.forEach((tree) => tree.update((((Math.sin(windSpeed * d.getTime() / (3 * Math.PI))) * (Math.sin(windSpeed * d.getTime() / 100))) * (windSpeed / 15) + windSpeed / 15) * windSpeed / 10 + 1));
+		_this.trees.forEach((tree) => tree.update(_this.wind.getForce(d.getTime() + ((tree.x / _this.ctx.canvas.width) * 100) + ((tree.y / _this.ctx.canvas.height) * 60))));
 	}
 }
 
 function Tree(ctx, options){
 	var _this = this;
-	_this.t = 0;
+
+	_this.salt = Math.random();
 
 	_this.kappa = 30;
 	_this.omega = 0;
-	_this.theta = 0;
+	_this.theta = ((_this.salt * 10)) * Math.PI / 180;
 
 	options = options === undefined ? {} : options;
 
@@ -44,26 +50,28 @@ function Tree(ctx, options){
 
 	_this.x = options.x === undefined ? 0 : options.x;
 	_this.y = options.y === undefined ? 0 : options.y;
-	_this.r = options.r === undefined ? phi : options.r;
+	_this.r = options.r === undefined ? phi + (_this.salt * 0.3) - 0.15 : options.r;
 	_this.scale = options.scale === undefined ? 1 : options.scale;
 
 	_this.draw = function (){
 		_this.ctx.save();
 		_this.ctx.translate(_this.x, _this.y);
-		_this.ctx.rotate(_this.theta);
+		_this.ctx.rotate(_this.theta / 2);
 		_this.ctx.scale(_this.scale, _this.scale);
 
 		_this.ctx.beginPath();
 		_this.ctx.moveTo(-0.5, _this.r * 0.25);
 		_this.ctx.lineTo(0.5, _this.r * 0.25);
+
+		_this.ctx.rotate(_this.theta);
 		_this.ctx.lineTo(0, -_this.r * 0.75);
+
 		_this.ctx.closePath();
 		_this.ctx.fill();
 		_this.ctx.restore();
 	};
 
 	_this.update = function (Fw){
-		_this.t += dt;
 		var tao = (_this.r * Fw * Math.sin(_this.theta + (Math.PI / 2))) - (_this.kappa * _this.theta) - (2 * _this.omega);
 		var alpha = tao / _this.r / _this.r;
 
@@ -71,6 +79,18 @@ function Tree(ctx, options){
 		_this.omega = _this.omega + (alpha * dt);
 
 		_this.draw();
+	}
+}
+
+function Wind(speed){
+	var _this = this;
+
+	_this.speed = speed ? speed : 5;
+
+	_this.getForce = function (t){
+		var speed = Math.sqrt(_this.speed / 3);
+		return (speed * Math.sin(t / Math.PI)) * ((speed * Math.sin(t / 100))) + (speed * 0.7);
+		// return (((Math.sin(_this.speed * t / (3 * Math.PI))) * (Math.sin(_this.speed * t / 100))) * (_this.speed / 15) + (_this.speed / 15)) * (_this.speed / 10) + 10;
 	}
 }
 
